99f30ea224523f4085e544f8faffd3063d145436
Shawn Crahen <shawn.j.crahen@gmail.com>
2020-08-30 16:52:52 -0400

    added colors to selection sort and insertion sort

--- application/Main.java
+++ application/Main.java
@@ -1,7 +1,5 @@
 package application;
 
-import java.util.ArrayList;
-
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
@@ -13,11 +11,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
@@ -29,17 +22,16 @@ import javafx.util.Duration;
 
 public class Main extends Application {
     
-    public static final int N = 200;
-    private Label[] arrayA, arrayB, arrayC, arrayD, mergeSortTempArray;
-    private Button shuffleButton, sortButton;
-    private TilePane boxA, boxB, boxC, boxD;
-    private ArrayList<TilePane> boxes;
+    public static final int N = 100;
+    private Label[] array, mergeSortTempArray;
+    private Button shuffleButton, sortButton, stopButton;
+    private TilePane box;
     private Timeline selSortAnimationController, insSortAnimationController, mergeSortAnimationController;
     private int iSelectionSort, jSelectionSort, minSelectionSort;
     private int iInsertionSort, jInsertionSort;
     private boolean unsortedItemFound;
     private Label itemToInsert;
-    private Rectangle m, n;
+    private Rectangle rectangle1, rectangle2, rectangle3;
     private int mergeSortSizeIndex, mergeSortFirst;
     private ComboBox<String> comboBox;
     private String algorithm = "";
@@ -47,61 +39,50 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-		    comboBox = new ComboBox<>();
-		    comboBox.getItems().addAll("Choose a Sorting Algorithm","Selection Sort","Insertion Sort","Merge Sort");
-		    comboBox.getSelectionModel().selectFirst();
-		    comboBox.setOnAction(e -> setAlgorithm());
+		    resetSortIndices();
 		    
-		    selSortAnimationController = new Timeline(new KeyFrame(Duration.millis(1), selSortStep));
+		    // Animation Controllers
+		    selSortAnimationController = new Timeline(new KeyFrame(Duration.millis(20), selSortStep));
 		    selSortAnimationController.setCycleCount(Timeline.INDEFINITE);
-		    iSelectionSort = 0;
-		    jSelectionSort = 1;
-		    minSelectionSort = 0;
-		    insSortAnimationController = new Timeline(new KeyFrame(Duration.millis(5), insSortStep));
+		    insSortAnimationController = new Timeline(new KeyFrame(Duration.millis(20), insSortStep));
 		    insSortAnimationController.setCycleCount(Timeline.INDEFINITE);
-		    iInsertionSort = 1;
-		    unsortedItemFound = false;
-		    mergeSortAnimationController = new Timeline(new KeyFrame(Duration.millis(50), mergeSortStep));
+		    mergeSortAnimationController = new Timeline(new KeyFrame(Duration.millis(20), mergeSortStep));
 		    mergeSortAnimationController.setCycleCount(Timeline.INDEFINITE);
-		    mergeSortSizeIndex = 1;
-		    mergeSortFirst = 0;
 		    
-		    boxA = getTilePane();
-		    boxB = getTilePane();
-		    boxC = getTilePane();
-		    boxD = getTilePane();
-		    boxes = new ArrayList<>();
-		    boxes.add(boxA);
-		    boxes.add(boxB);
-		    boxes.add(boxC);
-		    boxes.add(boxD);
+		    // TilePane
+		    box = getTilePane();
 		    
-		    arrayA = buildNewLabelArray();
-		    arrayB = buildNewLabelArray();
-		    arrayC = buildNewLabelArray();
-		    arrayD = buildNewLabelArray();
+		    // create array
+		    array = buildNewLabelArray();
 		    mergeSortTempArray = new Label[N];
 		    
+            // create controls
+            comboBox = new ComboBox<>();
+            comboBox.getItems().addAll("Choose a Sorting Algorithm","Selection Sort","Insertion Sort","Merge Sort");
+            comboBox.getSelectionModel().selectFirst();
+            comboBox.setOnAction(e -> setAlgorithm());          
+            shuffleButton = new Button("SHUFFLE");
+            shuffleButton.setOnAction(e -> shuffle());
+            sortButton = new Button("SORT");
+            sortButton.setOnAction(e -> playAnimation());
+            stopButton = new Button("STOP");
+            stopButton.setOnAction(e -> stopAnimation());
+		    
+            // create buttonBox
 		    HBox buttonBox = new HBox();
 		    buttonBox.setAlignment(Pos.CENTER);
-		    shuffleButton = new Button("SHUFFLE");
-		    shuffleButton.setOnAction(e -> shuffle());
-		    sortButton = new Button("SORT");
-		    sortButton.setOnAction(e -> playAnimations());
             HBox.setMargin(shuffleButton, new Insets(20));
             HBox.setMargin(sortButton, new Insets(20));
             HBox.setMargin(comboBox, new Insets(20));
+            HBox.setMargin(stopButton, new Insets(20));
 		    buttonBox.setPadding(new Insets(20));
-		    buttonBox.setMinWidth(900);
-		    buttonBox.setMaxWidth(900);
-		    buttonBox.getChildren().addAll(comboBox, shuffleButton, sortButton);
+		    buttonBox.setMinWidth(640);
+		    buttonBox.setMaxWidth(640);
+		    buttonBox.getChildren().addAll(comboBox, shuffleButton, sortButton, stopButton);
 		    
-			FlowPane root = new FlowPane(boxA, boxB, boxC, boxD, buttonBox);
-			FlowPane.setMargin(boxA, new Insets(20, 10, 10, 20));
-			FlowPane.setMargin(boxB, new Insets(20, 20, 10, 10));
-            FlowPane.setMargin(boxC, new Insets(10, 10, 20, 20));
-            FlowPane.setMargin(boxD, new Insets(10, 20, 20, 10));
-            redrawAll();
+			FlowPane root = new FlowPane(box, buttonBox);
+			FlowPane.setMargin(box, new Insets(20));
+            redraw();
 			
 			Scene scene = new Scene(root, 900, 600);
 			primaryStage.setScene(scene);
@@ -115,7 +96,7 @@ public class Main extends Application {
         algorithm = comboBox.getValue();
     }
 
-    private void playAnimations() {
+    private void playAnimation() {
         switch (algorithm) {
             case "Selection Sort":
                 selSortAnimationController.play(); break;
@@ -127,36 +108,46 @@ public class Main extends Application {
                 break;
         }
     }
+    
+    private void stopAnimation() {
+        switch (algorithm) {
+            case "Selection Sort":
+                selSortAnimationController.stop(); break;
+            case "Insertion Sort":
+                insSortAnimationController.stop(); break;
+            case "Merge Sort":
+                mergeSortAnimationController.stop(); break;
+            default:
+                break;
+        }
+    }
 
     private Label[] buildNewLabelArray() {
         Label[] a = new Label[N];
-        for (int i = 0; i < N; i++) {
-            Label label = new Label(null, new Rectangle(1, i, Color.BLACK));
-            label.setMinSize(1, 200);
-            label.setMaxSize(1, 200);
+        for (int i = 1; i <= N; i++) {
+            Label label = new Label(null, new Rectangle(4, i*2, Color.BLACK));
+            label.setMinSize(4, 200);
+            label.setMaxSize(4, 200);
             label.setAlignment(Pos.BOTTOM_CENTER);
-            a[i] = label;
+            a[i-1] = label;
         }
         return a;
     }
 
     private TilePane getTilePane() {
 	    TilePane box = new TilePane();
-        box = new TilePane(1,1);
+        box = new TilePane(4,1);
         box.setAlignment(Pos.CENTER);
-        box.setMinHeight(220);
-        box.setMaxHeight(220);
-        box.setMinWidth(420);
-        box.setMaxWidth(420);
-        box.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
+        box.setMinHeight(420);
+        box.setMaxHeight(420);
+        box.setMinWidth(860);
+        box.setMaxWidth(860);
         return box;
     }
 	
 	private void shuffle() {
-	    shuffle(arrayA);
-	    shuffle(arrayB);
-	    shuffle(arrayC);
-	    shuffle(arrayD);
+	    array = buildNewLabelArray();
+	    shuffle(array);
 	}
 
     private void shuffle(Label[] a) {
@@ -165,39 +156,30 @@ public class Main extends Application {
 	        int j = (int)(Math.random()*N);
 	        swap(a, i, j);
 	    }
-	    redrawAll();
+	    redraw();
     }
 	
-	private void redrawAll() {
-        for (TilePane box : boxes) {
-            box.getChildren().clear();
-        }
+	private void redraw() {
+	    box.getChildren().clear();
         for (int i = 0; i < N; i++) {
-            boxA.getChildren().add(arrayA[i]);
-            boxB.getChildren().add(arrayB[i]);
-            boxC.getChildren().add(arrayC[i]);
-            boxD.getChildren().add(arrayD[i]);
+            box.getChildren().add(array[i]);;
         }        
     }
 
     private void resetSortIndices() {
         iSelectionSort = 0;
+        jSelectionSort = 1;
+        minSelectionSort = 0;
         iInsertionSort = 1;
-        mergeSortFirst = 0;
+        unsortedItemFound = false;
         mergeSortSizeIndex = 1;
+        mergeSortFirst = 0;
     }
 
     private void swap(Label[] a, int i, int j) {
 	    Label temp = a[i];
 	    a[i] = a[j];
 	    a[j] = temp;
-	}
-	
-	private void redraw(TilePane box, Label[] a) {
-        box.getChildren().clear();
-        for (int i = 0; i < N; i++) {
-            box.getChildren().add(a[i]);
-        }
 	}
 	
 	private EventHandler<ActionEvent> mergeSortStep = e -> {
@@ -208,14 +190,14 @@ public class Main extends Application {
 	    }
 	    else {
 	        mergeSortStep();
-	        redraw(boxC, arrayC);
+	        redraw();
 	    }
 	};
 	
 	private void mergeSortStep() {
 	    int mid = Math.min(mergeSortFirst + mergeSortSizeIndex-1, N-1);
 	    int last = Math.min(mergeSortFirst + 2*mergeSortSizeIndex-1, N-1);
-	    merge(arrayC, mergeSortTempArray, mergeSortFirst, mid, last);
+	    merge(array, mergeSortTempArray, mergeSortFirst, mid, last);
 	    //adjust indexes
 	    if (mergeSortFirst < N - 2*mergeSortSizeIndex) {
 	        mergeSortFirst += 2*mergeSortSizeIndex;
@@ -277,38 +259,32 @@ public class Main extends Application {
             minSelectionSort = 0;
         }
         else {
-            //selSort(arrayA);
-            //redraw(boxA, arrayA);
+            rectangle1 = (Rectangle)array[iSelectionSort].getGraphic();
+            rectangle1.setFill(Color.RED);
             selSortStep();
         }
     };
-	
-//    private void selSort(Label[] a) {
-//        int minIndex = iSelectionSort;
-//        for (int j = iSelectionSort + 1; j < N; j++) {
-//            Rectangle m = (Rectangle)a[j].getGraphic();
-//            Rectangle n = (Rectangle)a[minIndex].getGraphic();
-//            if (m.getHeight() < n.getHeight()) {
-//                minIndex = j;
-//            }
-//        }
-//        swap(a, iSelectionSort, minIndex);
-//        iSelectionSort++;
-//    }
     
     private void selSortStep() {
         if (jSelectionSort == N) {
-            swap(arrayA, iSelectionSort, minSelectionSort);
-            redraw(boxA, arrayA);
+            swap(array, iSelectionSort, minSelectionSort);
+            rectangle1.setFill(Color.BLACK);
+            rectangle2.setFill(Color.BLACK);
+            rectangle3.setFill(Color.BLACK);
+            redraw();
             iSelectionSort++;
             minSelectionSort = iSelectionSort;
             jSelectionSort = iSelectionSort+1;
         }
         else {
-            Rectangle m = (Rectangle)arrayA[jSelectionSort].getGraphic();
-            Rectangle n = (Rectangle)arrayA[minSelectionSort].getGraphic();
-            if (m.getHeight() < n.getHeight()) {
+            if (rectangle2 != null && rectangle2.getFill() != Color.RED) rectangle2.setFill(Color.BLACK);
+            rectangle2 = (Rectangle)array[jSelectionSort].getGraphic();
+            rectangle3 = (Rectangle)array[minSelectionSort].getGraphic();
+            rectangle2.setFill(Color.GRAY);
+            if (rectangle2.getHeight() < rectangle3.getHeight()) {
+                rectangle2.setFill(Color.RED);
                 minSelectionSort = jSelectionSort;
+                rectangle3.setFill(Color.BLACK);
             }
             jSelectionSort++;
         }
@@ -316,70 +292,48 @@ public class Main extends Application {
     
     private EventHandler<ActionEvent> insSortStep = e -> {
         if (iInsertionSort == N) {
+            rectangle3 = (Rectangle)array[iInsertionSort-1].getGraphic();
+            rectangle3.setFill(Color.GRAY);
             insSortAnimationController.stop();
             iInsertionSort = 1;
             unsortedItemFound = false;
         }
         else {
-//            insSort(arrayB);
-//            redraw(boxB, arrayB);
             insertionSortStep();
         }
     };
     
     private void insertionSortStep() {
         if (unsortedItemFound) {
-            if (jInsertionSort > 0 && m.getHeight() < n.getHeight()) {
-                Label label = new Label(null, n);
-                label.setMinSize(1, 200);
-                label.setMaxSize(1, 200);
+            if (jInsertionSort > 0 && rectangle2.getHeight() < rectangle3.getHeight()) {
+                Label label = new Label(null, rectangle3);
+                label.setMinSize(4, 200);
+                label.setMaxSize(4, 200);
                 label.setAlignment(Pos.BOTTOM_CENTER);
-                arrayB[jInsertionSort] = label;
+                array[jInsertionSort] = label;
                 jInsertionSort--;
-                if (jInsertionSort > 0) n = (Rectangle)arrayB[jInsertionSort-1].getGraphic();
-                redraw(boxB, arrayB);
+                if (jInsertionSort > 0) rectangle3 = (Rectangle)array[jInsertionSort-1].getGraphic();
+                redraw();
             } else {
-                arrayB[jInsertionSort] = itemToInsert;
+                array[jInsertionSort] = itemToInsert;
+                Rectangle r = (Rectangle)itemToInsert.getGraphic();
+                r.setFill(Color.GRAY);
                 unsortedItemFound = false;
                 iInsertionSort++;
-                redraw(boxB, arrayB);
+                redraw();
             }
         } else {
-            m = (Rectangle)arrayB[iInsertionSort].getGraphic();
-            n = (Rectangle)arrayB[iInsertionSort-1].getGraphic();
-            if (m.getHeight() < n.getHeight()) {
+            rectangle2 = (Rectangle)array[iInsertionSort].getGraphic();
+            rectangle3 = (Rectangle)array[iInsertionSort-1].getGraphic();
+            if (rectangle2.getHeight() < rectangle3.getHeight()) {
                 unsortedItemFound = true;
-                itemToInsert = arrayB[iInsertionSort];
+                itemToInsert = array[iInsertionSort];
                 jInsertionSort = iInsertionSort;
             } else {
+                rectangle3.setFill(Color.GRAY);
                 iInsertionSort++;
             }
         }
-    }
-    
-    private void insSort(Label[] a) {
-        boolean found = false;
-        while(!found && iInsertionSort < N) {
-            Rectangle m = (Rectangle)a[iInsertionSort].getGraphic();
-            Rectangle n = (Rectangle)a[iInsertionSort-1].getGraphic();
-            if (m.getHeight() < n.getHeight()) {
-                found = true;
-                insert(a, iInsertionSort);
-                break;
-            }
-            iInsertionSort++;
-        }
-    }
-    
-    private void insert(Label[] a, int itemIndex) {
-        
-        Label itemToInsert = a[itemIndex];
-        int i = itemIndex;
-        while (i > 0 && ((Rectangle)itemToInsert.getGraphic()).getHeight() < ((Rectangle)a[i-1].getGraphic()).getHeight()) {
-            a[i] = a[i-1];
-            i--;
-        }
-        a[i] = itemToInsert;
     }
     
     public static void main(String[] args) {
